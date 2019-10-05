201392e1624f19c98c1e32b3ef5392599659016e
macro <zhenghong1991@126.com>
2019-10-05 15:53:08 +0800

    add zuul-proxy

---
+++ zuul-proxy/src/main/java/com/macro/cloud/ZuulProxyApplication.java
@@ -0,0 +1,17 @@
+package com.macro.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyApplication.class, args);
+    }
+
+}
---
+++ zuul-proxy/src/main/java/com/macro/cloud/filter/PreLogFilter.java
@@ -0,0 +1,56 @@
+package com.macro.cloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by macro on 2019/9/9.
+ */
+@Component
+public class PreLogFilter extends ZuulFilter {
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 过滤器类型，有pre、routing、post、error四种。
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 过滤器执行顺序，数值越小优先级越高。
+     */
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    /**
+     * 是否进行过滤，返回true会执行过滤。
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 自定义的过滤器逻辑，当shouldFilter()返回true时会执行。
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String host = request.getRemoteHost();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        LOGGER.info("Remote host:{},method:{},uri:{}", host, method, uri);
+        return null;
+    }
+}
