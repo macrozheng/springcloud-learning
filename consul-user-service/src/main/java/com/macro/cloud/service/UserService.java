3aaf191095da66643b1149662b19969a23ba8abb
macro <zhenghong1991@126.com>
2019-10-20 14:56:14 +0800

    add  consul demo

---
+++ consul-user-service/src/main/java/com/macro/cloud/service/UserService.java
@@ -0,0 +1,22 @@
+package com.macro.cloud.service;
+
+import com.macro.cloud.domain.User;
+
+import java.util.List;
+
+/**
+ * Created by macro on 2019/8/29.
+ */
+public interface UserService {
+    void create(User user);
+
+    User getUser(Long id);
+
+    void update(User user);
+
+    void delete(Long id);
+
+    User getByUsername(String username);
+
+    List<User> getUserByIds(List<Long> ids);
+}
